98565726c8a90c5b8cbb0bacc98cdf4b9164f354
afragorgen <gorgenafra@gmail.com>
2025-12-08 23:53:32 +0300

    Proje yapısı düzenlendi, home ve events route'ları eklendi, navbar bağlantıları düzeltildi, home.html ve events.html taslak sayfaları oluşturuldu, temel Bootstrap yapısı eklendi ve etkinlik sayfası için placeholder kart yapısı hazırlandı.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
